752100c9524bf4393558acdeaed10bd8eb2f91d3
Rohit J <rohit.jalagadugula@gmail.com>
2022-06-06 19:55:55 +0530

    Update Next config

--- meeting-calendar/next.config.js
+++ meeting-calendar/next.config.js
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  distDir: "build",
+  // distDir: "build",
   env: {
     TWILIO_VIDEO_APP_URL: process.env.TWILIO_VIDEO_APP_URL,
     // "https://video-app-4265-5450-dev.twil.io/?passcode=07385842655450"
